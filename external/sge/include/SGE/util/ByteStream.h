a9f4c3e9136fa4e75fd049bbd8b6f437671ae02a
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2026-06-28 09:09:00 -0700

    Added documentation comments to ByteStream class.

--- external/sge/include/SGE/util/ByteStream.h
+++ external/sge/include/SGE/util/ByteStream.h
@@ -15,6 +15,13 @@
 #endif
 
 namespace sge {
+/**
+ * Very useful utility class designed to make low-level byte writing/reading a little easier.
+ * This class is ideal for mixed-type sequential storage.
+ * 
+ * NOTE: If you use this class as a sequential stream, read in the exact same order that you write.
+ * This will prevent any corruption or crashes. 
+ */
 class ByteStream {
 private:
     unsigned char *m_data;
@@ -22,13 +29,29 @@ private:
     size_t m_writePointer;
     size_t m_readPointer;
 
+    /**
+     * Gets the next aligned position to start writing on.
+     * @param curPosition the current write index
+     * @param alignment the alignment of the object (calculated with alignof(T))
+     */
     size_t getAlignedPosition(size_t curPosition, size_t alignment) const;
 
+    /**
+     * Dynamic growth. Allows the ByteStream to reserve more allocated space if it runs out.
+     * @param newCapacity the new capacity limit of the ByteStream
+     */
     void reserve(size_t newCapacity);
 
 public:
+    /**
+     * Explicit parameterized constructor.
+     * @param capacity the initial capacity of the ByteStream 
+     */
     explicit ByteStream(size_t capacity);
     
+    /**
+     * Destructor. Frees all allocated memory of the ByteStream.
+     */
     ~ByteStream();
 
     ByteStream(const ByteStream &ByteStream) = delete;
@@ -37,35 +60,110 @@ public:
     ByteStream(ByteStream &&) noexcept;
     ByteStream& operator= (ByteStream &&) noexcept;
 
+    /**
+     * @return a modifiable type-erased pointer to the data
+     */
     void* data();
 
+    /**
+     * @return a non-modifable type-erased pointer to the data
+     */
     const void* data() const;
 
+    /**
+     * @return the amount of bytes written so far
+     */
     size_t size() const;
 
+    /**
+     * Sets the desired position to begin writing.
+     * 
+     * NOTE: May lead to corrupted data on the next write call if not done correctly
+     * @param position the desired position to write from
+     */
     void setWritePosition(size_t position);
 
+    /**
+     * @return the current read position of the ByteStream
+     */
     size_t getReadPosition() const;
 
+    /**
+     * Sets the desired position to begin reading.
+     * 
+     * NOTE: May lead to corrupted reads if not done correctly.
+     * @param position the desired position to read from
+     */
     void setReadPosition(size_t position);
 
+    /**
+     * @return the capacity of the ByteStream
+     */
     size_t capacity() const;
 
+    /**
+     * Resets the write and read pointers.
+     */
     void clear();
 
+    /**
+     * Templated function that allows one to write any type into the ByteStream.
+     * This function is ideal for small/primitive types.
+     * Use ByteStream::writeRawBytes() to write large payload data.
+     * 
+     * NOTE: This advances the write pointer by sizeof(T).
+     * @tparam T the type to write
+     * @param value the value to write into the ByteStream
+     * @return a handle index that can be used query the written data
+     */
     template <typename T>
     size_t write(const T &value);
 
+    /**
+     * Helper funtion that accepts a handle index (a return value from ByteStream::write()).
+     * @tparam T the type to search
+     * @param handleIndex the handle index of the data
+     * @return a modifiable reference of type T
+     */
     template <typename T>
     T& getByHandle(size_t handleIndex);
 
+    /**
+     * Helper funtion that accepts a handle index (a return value from ByteStream::write()).
+     * @tparam T the type to search
+     * @param handleIndex the handle index of the data
+     * @return a non-modifiable reference of type T
+     */
     template <typename T>
     const T& getByHandle(size_t handleIndex) const;
 
+    /**
+     * Ideal for writing large payload data into the ByteStream.
+     * However, it still works with simple primitive types too.
+     * 
+     * NOTE: This advances the write pointer by sizeInBytes.
+     * @param srcData pointer to the data to copy from
+     * @param sizeInBytes the size of the data to copy (if a passing class/struct, use sizeof() here)
+     */
     void writeRawBytes(const void *srcData, size_t sizeInBytes);
 
+    /**
+     * Ideal for reading large payload data from the ByteStream.
+     * However, it still works with simple primitive types too.
+     * 
+     * NOTE: This advances the read pointer by sizeInBytes.
+     * @param dest pointer to the address that the data should be copied to
+     * @param sizeInBytes the size (in bytes) to read
+     */
     void readRawBytes(void *dest, size_t sizeInBytes);
 
+    /**
+     * Reads a type from the ByteStream. Make sure you read exactly in the same order that you write.
+     * This ByteStream only works on sequential reads that follow the same write order.
+     * 
+     * NOTE: This advances the read pointer by sizeof(T).
+     * @return a value of type T stored in the ByteBuffer
+     */
     template <typename T>
     T read();
 }; // class ByteStream
