ddda1d021c849456398445e76db05e3968d1766a
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2026-07-18 14:57:13 -0700

    Updated sm::Quaternion class.
    Probably the biggest change is that now the in-place operations are driven by the out-of-place operations.

--- external/sm/include/SM/Quaternion.h
+++ external/sm/include/SM/Quaternion.h
@@ -1,75 +1,123 @@
 #ifndef SM_QUATERNION_H
 #define SM_QUATERNION_H
+#include <cstddef>
 #include <SM/Precision.h>
 #include <SM/Vec3.h>
 
 namespace sm {
 struct Quaternion {
-    sm::real w = 1;
-    sm::real x = 0;
-    sm::real y = 0;
-    sm::real z = 0;
-
-    Quaternion(sm::real w, sm::real x, sm::real y, sm::real z);
     
-    Quaternion& operator+= (const Quaternion &other);
+    union {
+        struct { sm::real x, y, z, w; };
+        sm::real data[4];
+    };
 
-    void normalize();
+    Quaternion(sm::real x, sm::real y, sm::real z, sm::real w);
     
-    static sm::real dot(const Quaternion &lhs, const Quaternion &rhs);
-    
-    Quaternion conjugated() const;
+    Quaternion& operator+=(const Quaternion &other);
 
-    Quaternion normalized() const;
+    Quaternion& operator*=(const Quaternion &other);
+
+    Quaternion& operator*=(real scalar);
+
+    Quaternion& rotateLocal(const Quaternion &rotationStep);
+
+    Quaternion& rotateGlobal(const Quaternion &rotationStep);
+
+    Quaternion& normalize();
+
+    [[nodiscard]] static sm::real dot(const Quaternion &lhs, const Quaternion &rhs);
+    
+    [[nodiscard]] Vec3 rotate(const Vec3 &rotation) const;
+
+    [[nodiscard]] Quaternion conjugated() const;
+
+    [[nodiscard]] Quaternion normalized() const;
+
+    // Ensures the entire struct stays exactly 16 bytes (4 floats * 4 bytes)
+    static_assert(sizeof(sm::Quaternion) == 16, "[sm::Quaternion]: Error. Quaternion size must be exactly 16 bytes!");
+
+    // Ensures the memory address of the array is identical to the x variable
+    static_assert(offsetof(Quaternion, data) == offsetof(Quaternion, x), "[sm::Quaternion]: Error. Array and x variable must align!");
 }; // class Quaternion
 
 // Math operator overload declarations.
-Quaternion operator* (const Quaternion &lhs, const Quaternion &rhs);
-Vec3 operator* (const Quaternion &quat, const Vec3 &v);
-Vec3 operator* (const Vec3 &v, const Quaternion &quat);
-Quaternion operator* (const Quaternion &quat, sm::real scalar);
-Quaternion operator* (sm::real scalar, const Quaternion &quat);
+[[nodiscard]] Quaternion operator+ (Quaternion lhs, const Quaternion &rhs);
+
+[[nodiscard]] Quaternion operator* (Quaternion lhs, const Quaternion &rhs);
+
+[[nodiscard]] Quaternion operator* (Quaternion quat, real scalar);
+[[nodiscard]] Quaternion operator* (real scalar, Quaternion quat);
+
+[[nodiscard]] Vec3 operator* (const Quaternion &quat, const Vec3 &v);
+[[nodiscard]] Vec3 operator* (const Vec3 &v, const Quaternion &quat);
 } // namespace sm
 
 // Math operator overload implementations
-inline sm::Quaternion sm::operator* (const sm::Quaternion &lhs, const sm::Quaternion &rhs)
+
+inline sm::Quaternion sm::operator+(sm::Quaternion lhs, const sm::Quaternion &rhs)
 {
-    return sm::Quaternion(
-        lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z,
-        lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y,
-        lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w + lhs.z * rhs.x,
-        lhs.w * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.w
-        );
+    lhs.x += rhs.x;
+    lhs.y += rhs.y;
+    lhs.z += rhs.z;
+    lhs.w += rhs.w;
+
+    return lhs;
+}
+
+inline sm::Quaternion sm::operator* (sm::Quaternion lhs, const sm::Quaternion &rhs)
+{
+    const sm::real x = lhs.w * rhs.x + lhs.x * rhs.w - lhs.y * rhs.z - lhs.z * rhs.y;
+    const sm::real y = lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w - lhs.z * rhs.x;
+    const sm::real z = lhs.w * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.w;
+    const sm::real w = lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z;
+
+    lhs.x = x;
+    lhs.y = y;
+    lhs.z = z;
+    lhs.w = w;
+
+    return lhs;
+}
+
+inline sm::Quaternion sm::operator* (sm::Quaternion quat, sm::real scalar)
+{
+    quat.x *= scalar;
+    quat.y *= scalar;
+    quat.z *= scalar;
+    quat.w *= scalar;
+
+    return quat;
+}
+
+inline sm::Quaternion sm::operator* (sm::real scalar, sm::Quaternion quat)
+{
+    quat.x *= scalar;
+    quat.y *= scalar;
+    quat.z *= scalar;
+    quat.w *= scalar;
+
+    return quat;
 }
 
 inline sm::Vec3 sm::operator* (const sm::Quaternion &quat, const sm::Vec3 &v)
 {
     sm::Vec3 qVec(quat.x, quat.y, quat.z);
 
-    sm::Vec3 cross1 = sm::Vec3::crossProduct(qVec, v);
-    sm::Vec3 cross2 = sm::Vec3::crossProduct(qVec, cross1);
+    sm::Vec3 intermediate = sm::Vec3::crossProduct(qVec, v) + (v * quat.w);
+    sm::Vec3 finalCross = sm::Vec3::crossProduct(qVec, intermediate);
 
-    return v + (cross1 * quat.w + cross2) * static_cast<sm::real>(2);
+    return v + finalCross * static_cast<sm::real>(2);
 }
 
-inline sm::Vec3 sm::operator* (const sm::Vec3 &v, const sm::Quaternion &mat)
+inline sm::Vec3 sm::operator* (const sm::Vec3 &v, const sm::Quaternion &quat)
 {
-    return mat * v;
-}
+    sm::Vec3 qVec(quat.x, quat.y, quat.z);
 
-inline sm::Quaternion sm::operator* (const sm::Quaternion &quat, sm::real scalar)
-{
-    return sm::Quaternion(
-        quat.w * scalar,
-        quat.x * scalar,
-        quat.y * scalar,
-        quat.z * scalar
-    );
-}
+    sm::Vec3 intermediate = sm::Vec3::crossProduct(qVec, v) + (v * quat.w);
+    sm::Vec3 finalCross = sm::Vec3::crossProduct(qVec, intermediate);
 
-inline sm::Quaternion sm::operator* (sm::real scalar, const sm::Quaternion &quat)
-{
-    return quat * scalar;
+    return v + finalCross * static_cast<sm::real>(2);
 }
 
 #endif // SM_QUATERNION_H
--- external/sm/src/Quaternion.cpp
+++ external/sm/src/Quaternion.cpp
@@ -1,51 +1,86 @@
 #include <SM/Quaternion.h>
 
-sm::Quaternion::Quaternion(sm::real w, sm::real x, sm::real y, sm::real z)
+sm::Quaternion::Quaternion(sm::real x, sm::real y, sm::real z, sm::real w)
 {
-    this->w = w;
     this->x = x;
     this->y = y;
     this->z = z;
+    this->w = w;
 }
 
 sm::Quaternion& sm::Quaternion::operator+= (const sm::Quaternion &other)
 {
-    this->w += other.w;
-    this->x += other.x;
-    this->y += other.y;
-    this->z += other.z;
+    (*this) = (*this) + other;
 
     return *this;
 }
 
-void sm::Quaternion::normalize()
+sm::Quaternion& sm::Quaternion::operator*=(const sm::Quaternion &other)
 {
-    sm::real mag = real_sqrt(this->w*this->w + this->x*this->x + this->y*this->y + this->z*this->z);
+    (*this) = (*this) * other;
+
+    return *this;
+}
+
+sm::Quaternion& sm::Quaternion::operator*=(sm::real scalar)
+{
+    (*this) = (*this) * scalar;
+
+    return *this;
+}
+
+sm::Quaternion& sm::Quaternion::rotateLocal(const sm::Quaternion &rotation)
+{
+    (*this) = (*this) * rotation;
+
+    return *this;
+}
+
+sm::Quaternion& sm::Quaternion::rotateGlobal(const sm::Quaternion &rotation)
+{
+    (*this) = rotation * (*this);
+
+    return *this;
+}
+
+sm::Quaternion& sm::Quaternion::normalize()
+{
+    sm::real mag = this->x*this->x + this->y*this->y + this->z*this->z + this->w*this->w;
 
     if (mag > static_cast<sm::real>(0))
     {
-        sm::real invMag = static_cast<sm::real>(1) / mag;
-
-        this->w *= invMag;
+        sm::real invMag = static_cast<sm::real>(1) / real_sqrt(mag);
         this->x *= invMag;
         this->y *= invMag;
         this->z *= invMag;
+        this->w *= invMag;
     }
+    else
+    {
+        this->w = 1;
+    }
+
+    return *this;
 }
 
 sm::real sm::Quaternion::dot(const sm::Quaternion &lhs, const sm::Quaternion &rhs)
 {
-    return (lhs.w * rhs.w) + (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z);
+    return (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z) + (lhs.w * rhs.w);
+}
+
+sm::Vec3 sm::Quaternion::rotate(const sm::Vec3 &rotation) const
+{
+    return (*this) * rotation;
 }
 
 sm::Quaternion sm::Quaternion::conjugated() const
 {
-    return Quaternion(this->w, -this->x, -this->y, -this->z);
+    return Quaternion(-this->x, -this->y, -this->z, this->w);
 }
 
 sm::Quaternion sm::Quaternion::normalized() const
 {
-    sm::Quaternion result(this->w, this->x, this->y, this->z);
+    sm::Quaternion result(this->x, this->y, this->z, this->w);
 
     result.normalize();
 
