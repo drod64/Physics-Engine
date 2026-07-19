906be78c5c45068fa5440da5292e9bb9ebb90b0b
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2026-07-19 00:24:41 -0700

    Updated sm::Quaternion.
    Added integrate().

--- external/sm/include/SM/Quaternion.h
+++ external/sm/include/SM/Quaternion.h
@@ -1,18 +1,21 @@
 #ifndef SM_QUATERNION_H
 #define SM_QUATERNION_H
 #include <cstddef>
+#include <cstdint>
 #include <SM/Precision.h>
 #include <SM/Vec3.h>
 
 namespace sm {
+enum class CoordinateFrame : uint8_t {Base, Internal};
+
 struct Quaternion {
-    
     union {
         struct { sm::real x, y, z, w; };
         sm::real data[4];
     };
 
-    Quaternion(sm::real x, sm::real y, sm::real z, sm::real w);
+    explicit Quaternion(real nx = static_cast<real>(0), real ny = static_cast<real>(0),
+                        real nz = static_cast<real>(0), real nw = static_cast<real>(1));
     
     Quaternion& operator+=(const Quaternion &other);
 
@@ -20,15 +23,15 @@ struct Quaternion {
 
     Quaternion& operator*=(real scalar);
 
-    Quaternion& rotateLocal(const Quaternion &rotationStep);
-
-    Quaternion& rotateGlobal(const Quaternion &rotationStep);
+    Quaternion& integrate(const Vec3 &w, real scalar, CoordinateFrame frame = CoordinateFrame::Base);
 
     Quaternion& normalize();
 
     [[nodiscard]] static sm::real dot(const Quaternion &lhs, const Quaternion &rhs);
     
-    [[nodiscard]] Vec3 rotate(const Vec3 &rotation) const;
+    [[nodiscard]] Vec3 transform(const Vec3 &rotation) const;
+
+    [[nodiscard]] Vec3 transfromInverse(const Vec3 &rotation) const;
 
     [[nodiscard]] Quaternion conjugated() const;
 
@@ -67,8 +70,8 @@ inline sm::Quaternion sm::operator+(sm::Quaternion lhs, const sm::Quaternion &rh
 
 inline sm::Quaternion sm::operator* (sm::Quaternion lhs, const sm::Quaternion &rhs)
 {
-    const sm::real x = lhs.w * rhs.x + lhs.x * rhs.w - lhs.y * rhs.z - lhs.z * rhs.y;
-    const sm::real y = lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w - lhs.z * rhs.x;
+    const sm::real x = lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y;
+    const sm::real y = lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w + lhs.z * rhs.x;
     const sm::real z = lhs.w * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.w;
     const sm::real w = lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z;
 
@@ -104,20 +107,18 @@ inline sm::Vec3 sm::operator* (const sm::Quaternion &quat, const sm::Vec3 &v)
 {
     sm::Vec3 qVec(quat.x, quat.y, quat.z);
 
-    sm::Vec3 intermediate = sm::Vec3::crossProduct(qVec, v) + (v * quat.w);
-    sm::Vec3 finalCross = sm::Vec3::crossProduct(qVec, intermediate);
+    const sm::Vec3 t = sm::Vec3::crossProduct(qVec, v) * static_cast<sm::real>(2);
 
-    return v + finalCross * static_cast<sm::real>(2);
+    return v + (t * quat.w) + sm::Vec3::crossProduct(qVec, t);
 }
 
 inline sm::Vec3 sm::operator* (const sm::Vec3 &v, const sm::Quaternion &quat)
 {
     sm::Vec3 qVec(quat.x, quat.y, quat.z);
 
-    sm::Vec3 intermediate = sm::Vec3::crossProduct(qVec, v) + (v * quat.w);
-    sm::Vec3 finalCross = sm::Vec3::crossProduct(qVec, intermediate);
+    const sm::Vec3 t = sm::Vec3::crossProduct(qVec, v) * static_cast<sm::real>(2);
 
-    return v + finalCross * static_cast<sm::real>(2);
+    return v + (t * quat.w) + sm::Vec3::crossProduct(qVec, t);
 }
 
 #endif // SM_QUATERNION_H
--- external/sm/src/Quaternion.cpp
+++ external/sm/src/Quaternion.cpp
@@ -1,12 +1,8 @@
 #include <SM/Quaternion.h>
 
-sm::Quaternion::Quaternion(sm::real x, sm::real y, sm::real z, sm::real w)
-{
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-}
+sm::Quaternion::Quaternion(sm::real nx, sm::real ny, sm::real nz, sm::real nw) :
+x(nx), y(ny), z(nz), w(nw)
+{}
 
 sm::Quaternion& sm::Quaternion::operator+= (const sm::Quaternion &other)
 {
@@ -29,27 +25,50 @@ sm::Quaternion& sm::Quaternion::operator*=(sm::real scalar)
     return *this;
 }
 
-sm::Quaternion& sm::Quaternion::rotateLocal(const sm::Quaternion &rotation)
+sm::Quaternion& sm::Quaternion::integrate(const sm::Vec3 &w, sm::real scalar, sm::CoordinateFrame frame)
 {
-    (*this) = (*this) * rotation;
+    const sm::real halfX = w.x * scalar * static_cast<sm::real>(0.5);
+    const sm::real halfY = w.y * scalar * static_cast<sm::real>(0.5);
+    const sm::real halfZ = w.z * scalar * static_cast<sm::real>(0.5);
 
-    return *this;
-}
+    const sm::real prevX = this->x;
+    const sm::real prevY = this->y;
+    const sm::real prevZ = this->z;
+    const sm::real prevW = this->w;
 
-sm::Quaternion& sm::Quaternion::rotateGlobal(const sm::Quaternion &rotation)
-{
-    (*this) = rotation * (*this);
+    switch (frame)
+    {
+        case sm::CoordinateFrame::Internal:
+            this->x += ( prevW * halfX + prevZ * halfY - prevY * halfZ);
+            this->y += (-prevZ * halfX + prevW * halfY + prevX * halfZ);
+            this->z += ( prevY * halfX - prevX * halfY + prevW * halfZ);
+            this->w += (-prevX * halfX - prevY * halfY - prevZ * halfZ);
+            break; 
+
+        case sm::CoordinateFrame::Base:
+        default:
+            this->x += ( halfX * prevW - halfY * prevZ - halfZ * prevY);
+            this->y += (-halfX * prevZ + halfY * prevW - halfZ * prevX);
+            this->z += ( halfX * prevY - halfY * prevX + halfZ * prevW);
+            this->w += (-halfX * prevX - halfY * prevY - halfZ * prevZ);
+            break;
+    }
+
+    this->normalize();
 
     return *this;
 }
 
 sm::Quaternion& sm::Quaternion::normalize()
 {
-    sm::real mag = this->x*this->x + this->y*this->y + this->z*this->z + this->w*this->w;
+    sm::real magSqr = this->x*this->x + this->y*this->y + this->z*this->z + this->w*this->w;
 
-    if (mag > static_cast<sm::real>(0))
+    if (magSqr > static_cast<sm::real>(0.00001))
     {
-        sm::real invMag = static_cast<sm::real>(1) / real_sqrt(mag);
+        sm::real invMag = static_cast<sm::real>(1) / real_sqrt(magSqr);
+
+        invMag = invMag * (static_cast<sm::real>(1.5) - (static_cast<sm::real>(0.5) * magSqr * invMag * invMag));
+
         this->x *= invMag;
         this->y *= invMag;
         this->z *= invMag;
@@ -57,7 +76,10 @@ sm::Quaternion& sm::Quaternion::normalize()
     }
     else
     {
-        this->w = 1;
+        this->x = static_cast<sm::real>(0);
+        this->y = static_cast<sm::real>(0);
+        this->z = static_cast<sm::real>(0);
+        this->w = static_cast<sm::real>(1);
     }
 
     return *this;
@@ -68,9 +90,18 @@ sm::real sm::Quaternion::dot(const sm::Quaternion &lhs, const sm::Quaternion &rh
     return (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z) + (lhs.w * rhs.w);
 }
 
-sm::Vec3 sm::Quaternion::rotate(const sm::Vec3 &rotation) const
+sm::Vec3 sm::Quaternion::transform(const sm::Vec3 &rotation) const
 {
     return (*this) * rotation;
+}
+
+sm::Vec3 sm::Quaternion::transfromInverse(const sm::Vec3 &rotation) const
+{
+    sm::Vec3 qVec(-this->x, -this->y, -this->z);
+
+    const sm::Vec3 t = sm::Vec3::crossProduct(qVec, rotation) * static_cast<sm::real>(2);
+
+    return rotation + (t * this->w) + sm::Vec3::crossProduct(qVec, t);
 }
 
 sm::Quaternion sm::Quaternion::conjugated() const
